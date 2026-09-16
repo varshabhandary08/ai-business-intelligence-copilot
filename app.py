4750e12dfd6b603fa17ed7c60f878e82820b8552
varshabhandary08 <varshabhandary08@gmail.com>
2026-09-16 21:28:18 +0530

    Configure Groq secret for Streamlit deployment

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 client = Groq(
-    api_key=os.getenv("GROQ_API_KEY")
+    client = Groq(
+    api_key=st.secrets["GROQ_API_KEY"]
+)
 )
 
 # Page Title
